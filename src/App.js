e8f25188a33afa2a4655984b06d191257f71e562
lnkz80 <lnkz80@gmail.com>
2023-06-02 14:51:37 +0300

    Scnd push

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
-const AppHeader = (usr) => <h2 className="appHeader">Hello!</h2>;
+const AppHeader = ({ uname }) => <h2 className='appHeader'>Hello, {uname}!</h2>;
 
 function App() {
   return (
     <div className='App'>
-      <AppHeader usr='Hike' />
-      <button className='btn btn-primary'>Click</button>      
+      <AppHeader uname='Hike' />
+      <button className='btn btn-primary'>Click</button>
     </div>
   );
 }
