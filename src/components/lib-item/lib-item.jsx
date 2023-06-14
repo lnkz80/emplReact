e53de10053d1122aa6521aa445b03fe471303e55
lnkz80 <lnkz80@gmail.com>
2023-06-14 11:23:08 +0300

    Refactoring css for lib-item

--- src/components/lib-item/lib-item.jsx
+++ src/components/lib-item/lib-item.jsx
@@ -1,18 +1,17 @@
-
+import './lib-item.css'
 
 const LibItem = ({i, item}) => {
     const {name, type, location, user} = item;
     return(
-        <tr>
+        <tr className="lib-item">
             <td>{i}</td>
             <td>{name}</td>
             <td>{type}</td>
             <td>{location}</td>
             <td>{user}</td>
-            <td>
-                {/* //! NEED 2 ADD CLASSES WITH HOVER COLORS ON THIS BUTTONS (btnEdit, btnTrash)  */}
-                <a href="\"><i className="fa-regular fa-pen-to-square"></i></a>
-                <a href="\"><i className="fa-regular fa-trash-can"></i></a>
+            <td>                
+                <a className="lib-item__edit" href="\"><i className="fa-regular fa-pen-to-square"></i></a>
+                <a className="lib-item__delete" href="\"><i className="fa-regular fa-trash-can"></i></a>
             </td>
         </tr>
     );
