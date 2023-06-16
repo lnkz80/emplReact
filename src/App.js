f26cc1094bda03b7adb97e43b069ec6417c2edfc
lnkz80 <lnkz80@gmail.com>
2023-06-16 21:56:07 +0300

    Training with setState in Counter component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import AppHeader from './components/app-header/app-header'
 import LibControlPanel from './components/lib-control-panel/lib-control-panel';
 import LibListItems from './components/lib-list-items/lib-list-items';
+import Counter from './components/counter/counter';
+
 
 const App = () => {
   return (
@@ -9,6 +11,7 @@ const App = () => {
       <AppHeader title="Devices library" icon="fa-solid fa-book" />       
       <LibControlPanel />
       <LibListItems />
+      <Counter start={0} />
     </div>
   );
 }
---
+++ src/components/counter/counter.jsx
@@ -0,0 +1,50 @@
+import { Component } from 'react';
+
+class Counter extends Component {
+    constructor(props){
+      super();
+      this.maxPoint = 5;
+      this.minPoint = -5;
+      this.state ={
+        count: props.start,
+      }
+    }
+  
+    countPlus = () => this.setState(state => (    
+        {
+          count: state.count < this.maxPoint ? state.count + 1 : state.count,
+        }
+      )
+    );
+  
+    countMinus = () => this.setState(state => (
+        {
+          count: state.count > this.minPoint ? state.count - 1 : state.count,
+        }
+      )
+    );
+  
+     reset = () => this.setState(state => ({count: this.props.start})) ;
+  
+     round = () => this.setState(state => (
+          {
+            count: Math.round(Math.random() * (this.maxPoint - this.minPoint) + this.minPoint)
+          }
+        )
+      ) ;
+    
+    render(){
+      return (
+        <div>
+          <button onClick={ this.countMinus }>-</button>
+          <span >{this.state.count}</span>
+          <button onClick={ this.countPlus }>+</button>
+          <button onClick={ this.round }>RND</button>
+          <button onClick={ this.reset }>RST</button>
+        </div>
+      );
+    }
+  
+  }
+
+  export default Counter;
