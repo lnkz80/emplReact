6961c36e68d8b78514f0b933623aa38dff1317a4
lnkz <lnkz80@gmail.com>
2023-06-26 22:18:59 +0300

    NewItem Component - IN PRGRS

--- src/components/lib-control-panel/lib-control-panel.jsx
+++ src/components/lib-control-panel/lib-control-panel.jsx
@@ -1,11 +1,19 @@
 import './lib-control-panel.css'
+import LibNewItem from '../lib-new-item/lib-new-item'; 
 
 const LibControlPanel = () => {
+    const showPanel = true;
     return (
-        <div className="lib-control-panel app-brd">            
-            <i className="fa-sharp fa-solid fa-circle-plus"></i>
-            <i className="fa-solid fa-filter"></i>
-            <input type="text" placeholder='Пошук...' />
+        <div className="lib-control-panel app-brd">
+            <div className='lib-control-panel__afs-line'>
+                <i className="fa-sharp fa-solid fa-circle-plus"></i>
+                <i className="fa-solid fa-filter"></i>
+                <input type="text" placeholder='Пошук...' />                
+            </div>
+            {
+                showPanel&&<LibNewItem />
+            }
+            {/* <LibNewItem /> */}
         </div>
     );
 }
---
+++ src/components/lib-new-item/lib-new-item.jsx
@@ -0,0 +1,11 @@
+import "./lib-new-item.css"
+
+const LibNewItem = () => {
+    return (
+        <div>
+            NewItem
+        </div>
+    )
+};
+
+export default LibNewItem;
